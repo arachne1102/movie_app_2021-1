9f9fd35488d21206252b6216f42d48907f2c59f6
BlowF1y <blowfly0258@gmail.com>
2021-09-18 18:00:27 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+const foodLike = [
+  {
+    name: 'spaghetti',
+    image: 'https://www.tefal.co.kr/medias/?context=bWFzdGVyfHJvb3R8MjUyMTF8aW1hZ2UvanBlZ3xoZmYvaGFlLzE0MDcwMjU0NTY3NDU0LmpwZ3w0NmQ1Yjg3OTY0YzhlNDBlMjBiNzU5ZTE2N2RkNzBlZGRjMmRhYWJiMGQyOGIxMDEwZmM5MjFhNmM5OTYxNmE5'
+  },
+  {
+    name: 'pizza',
+    image: 'https://cdn.imweb.me/thumbnail/20170922/59c3e2b67bbb3.png'
+  },
+  {
+    name: 'tomato',
+    image: 'https://health.chosun.com/site/data/img_dir/2021/01/12/2021011202231_0.jpg'
+  }
+]
+
+function renderFood(foo) {
+  return <Food name={foo.name} picture={foo.image} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        foodLike.map(renderFood)
+      }
     </div>
   );
 }
 
-export default App;
+function Food({name, picture}) {
+  // {fav} = props
+  return (
+    <div>
+      <h3>I love {name} </h3>
+      <img src={picture} />
+    </div>
+  )
+}
+
+export default App
